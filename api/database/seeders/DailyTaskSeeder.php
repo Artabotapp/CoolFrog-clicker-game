fcd8e78762fe0f6f4240db63dfb101772da9036b
Artabotapp <sullivan.richard991@gmail.com>
2025-01-24 13:40:03 +0330

    Update DailyTaskSeeder.php

--- api/database/seeders/DailyTaskSeeder.php
+++ api/database/seeders/DailyTaskSeeder.php
@@ -35,7 +35,7 @@ class DailyTaskSeeder extends Seeder
             [
                 'id' => 4,
                 'name' => 'Day 4',
-                'reward_coins' => 5000,
+                'reward_coins' => 5500,
                 'required_login_streak' => 4,
             ],
             [
@@ -53,63 +53,87 @@ class DailyTaskSeeder extends Seeder
             [
                 'id' => 7,
                 'name' => 'Day 7',
-                'reward_coins' => 25_000,
+                'reward_coins' => 20_000,
                 'required_login_streak' => 7,
             ],
             [
                 'id' => 8,
                 'name' => 'Day 8',
-                'reward_coins' => 50_000,
+                'reward_coins' => 35_000,
                 'required_login_streak' => 8,
             ],
             [
                 'id' => 9,
                 'name' => 'Day 9',
-                'reward_coins' => 75_000,
+                'reward_coins' => 55_000,
                 'required_login_streak' => 9,
             ],
             [
                 'id' => 10,
                 'name' => 'Day 10',
-                'reward_coins' => 100_000,
+                'reward_coins' => 85_000,
                 'required_login_streak' => 10,
             ],
             [
                 'id' => 11,
                 'name' => 'Day 11',
-                'reward_coins' => 250_000,
+                'reward_coins' => 100_000,
                 'required_login_streak' => 11,
             ],
             [
                 'id' => 12,
                 'name' => 'Day 12',
-                'reward_coins' => 500_000,
+                'reward_coins' => 150_000,
                 'required_login_streak' => 12,
             ],
             [
                 'id' => 13,
                 'name' => 'Day 13',
-                'reward_coins' => 750_000,
+                'reward_coins' => 250_000,
                 'required_login_streak' => 13,
             ],
             [
                 'id' => 14,
                 'name' => 'Day 14',
-                'reward_coins' => 1_000_000,
+                'reward_coins' => 400_000,
                 'required_login_streak' => 14,
             ],
             [
                 'id' => 15,
                 'name' => 'Day 15',
-                'reward_coins' => 1_250_000,
+                'reward_coins' => 750_000,
                 'required_login_streak' => 15,
             ],
             [
                 'id' => 16,
                 'name' => 'Day 16',
-                'reward_coins' => 1_500_000,
+                'reward_coins' => 900_000,
                 'required_login_streak' => 16,
             ],
+            [
+                'id' => 17,
+                'name' => 'Day 17',
+                'reward_coins' => 1_100_000,
+                'required_login_streak' => 17,
+            ],
+            [
+                'id' => 18,
+                'name' => 'Day 18',
+                'reward_coins' => 1_400_000,
+                'required_login_streak' => 18,
+            ],
+            [
+                'id' => 19,
+                'name' => 'Day 19',
+                'reward_coins' => 1_800_000,
+                'required_login_streak' => 19,
+            ],
+            [
+                'id' => 20,
+                'name' => 'Day 20',
+                'reward_coins' => 2_400_000,
+                'required_login_streak' => 20,
+            ],            
         ];
 
         foreach ($dailyTasks as $task) {
