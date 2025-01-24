c2a8ba3116692ac0ed23720646a145babc61aa64
Artabotapp <sullivan.richard991@gmail.com>
2025-01-24 13:41:29 +0330

    Update LevelSeeder.php

--- api/database/seeders/LevelSeeder.php
+++ api/database/seeders/LevelSeeder.php
@@ -13,11 +13,11 @@ class LevelSeeder extends Seeder
     public function run(): void
     {
         $levels = [
-            ['level' => 1, 'name' => 'Starter', 'from_balance' => 5_000, 'to_balance' => 15_000],
-            ['level' => 2, 'name' => 'Adventurer', 'from_balance' => 15_000, 'to_balance' => 50_000],
-            ['level' => 3, 'name' => 'Explorer', 'from_balance' => 50_000, 'to_balance' => 150_000],
-            ['level' => 4, 'name' => 'Traveler', 'from_balance' => 150_000, 'to_balance' => 500_000],
-            ['level' => 5, 'name' => 'Boss', 'from_balance' => 500_000, 'to_balance' => 1_000_000],
+            ['level' => 1, 'name' => 'Starter', 'from_balance' => 5_000, 'to_balance' => 150_000],
+            ['level' => 2, 'name' => 'Adventurer', 'from_balance' => 150_000, 'to_balance' => 500_000],
+            ['level' => 3, 'name' => 'Explorer', 'from_balance' => 500_000, 'to_balance' => 1_000_000],
+            ['level' => 4, 'name' => 'Traveler', 'from_balance' => 1_000_000, 'to_balance' => 5_000_000],
+            ['level' => 5, 'name' => 'Boss', 'from_balance' => 5_000_000, 'to_balance' => 10_000_000],
         ];
 
         foreach ($levels as $level) {
